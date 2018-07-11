f659223390c96cd8956adc5c0bcdd8c1ccf1aca7
vadym <akademiya.adw@gmail.com>
2018-07-11 22:34:48 +0300

    update chronometer

---
+++ app/src/main/java/com/example/user/bestfriendskotlin/kido/Chronometer.kt
@@ -0,0 +1,41 @@
+package com.example.user.bestfriendskotlin.kido
+
+import android.os.Handler
+import android.os.Looper
+import java.util.*
+
+object Chronometer {
+    var base: Long = 0
+    var nextBeep: Long = 0
+
+    private lateinit var tickListener: () -> Unit
+    private lateinit var scheduledTask: TimerTask
+    private val timer = Timer()
+
+    fun setOnTick(whatIDo: () -> Unit) {
+        tickListener = whatIDo
+    }
+
+    fun start() {
+        scheduledTask = object : TimerTask() {
+            override fun run() {
+                Handler(Looper.getMainLooper()).post {
+                    tickListener()
+                }
+            }
+        }
+        timer.scheduleAtFixedRate(scheduledTask, 1000, 1000)
+    }
+
+    fun stop() {
+        scheduledTask.cancel()
+        nextBeep = 0
+    }
+
+    fun reset() {
+        scheduledTask.cancel()
+        nextBeep = 0
+        base = 0
+    }
+
+}
--- app/src/main/java/com/example/user/bestfriendskotlin/kido/PersonView.kt
+++ app/src/main/java/com/example/user/bestfriendskotlin/kido/PersonView.kt
@@ -7,17 +7,19 @@ import android.os.SystemClock
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.text.TextUtils
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
 import com.example.user.bestfriendskotlin.MainActivity
 import com.example.user.bestfriendskotlin.R
+import com.example.user.bestfriendskotlin.kido.Chronometer.nextBeep
 import com.example.user.bestfriendskotlin.kido.adapter.PersonAdapter
 import com.example.user.bestfriendskotlin.kido.database.SqliteDatabase
 import com.example.user.bestfriendskotlin.tracker
 import kotlinx.android.synthetic.main.view_kido.*
 
-class PersonView : MainActivity(), IPersonView {
+class PersonView : MainActivity() {
     private lateinit var allPerson: List<Person>
     private lateinit var listPersonEmpty: RelativeLayout
     private lateinit var database: SqliteDatabase
@@ -95,59 +97,56 @@ class PersonView : MainActivity(), IPersonView {
 
     private fun chronometer() {
         val mp = MediaPlayer.create(this, R.raw.ton)
-        var flag3 = false
-        var flag7 = false
-        var flag12 = false
-        var flag21 = false
-        var flag40 = false
 
         start.setOnClickListener { _ ->
-            chronometer.base = SystemClock.elapsedRealtime()
-            chronometer.start()
+            Chronometer.base = SystemClock.elapsedRealtime()
+            Chronometer.start()
         }
 
-        chronometer.onChronometerTickListener = Chronometer.OnChronometerTickListener {
-            val elapsedMillis = SystemClock.elapsedRealtime() - chronometer.base
 
-            if (elapsedMillis >= 180000 && flag3 == false) {
-                Toast.makeText(this, "Прошло 3 минуты", Toast.LENGTH_SHORT).show()
-                mp.start()
-                flag3 = true
-            } else if(elapsedMillis >= 420000 && flag7 == false) {
-                Toast.makeText(this, "Прошло 7 минут", Toast.LENGTH_SHORT).show()
-                mp.start()
-                flag7 = true
-            } else if(elapsedMillis >= 720000 && flag12 == false) {
-                Toast.makeText(this, "Прошло 12 минут", Toast.LENGTH_SHORT).show()
-                mp.start()
-                flag12 = true
-            } else if(elapsedMillis >= 1260000 && flag21 == false) {
-                Toast.makeText(this, "Прошло 21 минута", Toast.LENGTH_SHORT).show()
-                mp.start()
-                flag21 = true
-            } else if(elapsedMillis >= 2400000 && flag40 == false) {
-                Toast.makeText(this, "Прошло 40 минут", Toast.LENGTH_SHORT).show()
-                mp.start()
-                flag40 = true
+        Chronometer.setOnTick {
+            val now = SystemClock.elapsedRealtime()
+            val elapsedMillis = now - Chronometer.base
+
+            chronometer.text = DateUtils.formatElapsedTime(elapsedMillis / 1000)
+            if (elapsedMillis < Chronometer.nextBeep)
+                return@setOnTick
+
+            when {
+                elapsedMillis >= 2400000 -> {
+                    Toast.makeText(this, "Прошло 40 минут", Toast.LENGTH_SHORT).show()
+                    mp.start()
+                }
+                elapsedMillis >= 1260000 -> {
+                    Toast.makeText(this, "Прошло 21 минута", Toast.LENGTH_SHORT).show()
+                    mp.start()
+                    nextBeep = 2400000
+                }
+                elapsedMillis >= 720000 -> {
+                    Toast.makeText(this, "Прошло 12 минут", Toast.LENGTH_SHORT).show()
+                    mp.start()
+                    nextBeep = 1260000
+                }
+                elapsedMillis >= 420000 -> {
+                    Toast.makeText(this, "Прошло 7 минут", Toast.LENGTH_SHORT).show()
+                    mp.start()
+                    nextBeep = 720000
+                }
+                elapsedMillis >= 180000 -> {
+                    Toast.makeText(this, "Прошло 3 минуты", Toast.LENGTH_SHORT).show()
+                    mp.start()
+                    nextBeep = 420000
+                }
             }
         }
 
         stop.setOnClickListener { _ ->
-            chronometer.stop()
-            flag3 = false
-            flag7 = false
-            flag12 = false
-            flag21 = false
-            flag40 = false
+            Chronometer.stop()
         }
 
         reset.setOnClickListener { _ ->
-            chronometer.base = SystemClock.elapsedRealtime()
-            flag3 = false
-            flag7 = false
-            flag12 = false
-            flag21 = false
-            flag40 = false
+            Chronometer.reset()
+            Chronometer.base = SystemClock.elapsedRealtime()
         }
     }
 
