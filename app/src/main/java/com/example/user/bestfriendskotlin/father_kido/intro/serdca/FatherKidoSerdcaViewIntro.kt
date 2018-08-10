291414ceb82a9f17cb8c83b365e85ded830bf628
vadym <akademiya.adw@gmail.com>
2018-08-10 11:07:55 +0300

    add prayers serdca - finish

--- app/src/main/java/com/example/user/bestfriendskotlin/father_kido/intro/serdca/FatherKidoSerdcaViewIntro.kt
+++ app/src/main/java/com/example/user/bestfriendskotlin/father_kido/intro/serdca/FatherKidoSerdcaViewIntro.kt
@@ -5,14 +5,11 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.widget.LinearLayout
-import com.example.user.bestfriendskotlin.AndroidApplication
 import com.example.user.bestfriendskotlin.MainActivity
 import com.example.user.bestfriendskotlin.R
-import com.example.user.bestfriendskotlin.father_kido.intro.ReadTextFromFile
 import com.example.user.bestfriendskotlin.tracker
-import java.io.InputStream
 
-class FatherKidoSerdcaViewIntro : MainActivity(), ReadTextFromFile {
+class FatherKidoSerdcaViewIntro : MainActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,13 +53,20 @@ class FatherKidoSerdcaViewIntro : MainActivity(), ReadTextFromFile {
         kido.add(KidoSerdca(getString(R.string.pr_serdca_18), getString(R.string.pr_serdca_18t)))
         kido.add(KidoSerdca(getString(R.string.pr_serdca_19), getString(R.string.pr_serdca_19t)))
         kido.add(KidoSerdca(getString(R.string.pr_serdca_20), getString(R.string.pr_serdca_20t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_21), getString(R.string.pr_serdca_21t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_22), getString(R.string.pr_serdca_22t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_23), getString(R.string.pr_serdca_23t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_24), getString(R.string.pr_serdca_24t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_25), getString(R.string.pr_serdca_25t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_26), getString(R.string.pr_serdca_26t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_27), getString(R.string.pr_serdca_27t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_28), getString(R.string.pr_serdca_28t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_29), getString(R.string.pr_serdca_29t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_30), getString(R.string.pr_serdca_30t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_31), getString(R.string.pr_serdca_31t)))
+        kido.add(KidoSerdca(getString(R.string.pr_serdca_32), getString(R.string.pr_serdca_32t)))
 
         val adapter = KidoSerdcaAdapter(kido)
         rv.adapter = adapter
-    }
-
-    override fun readTextFromFile(textFile: String): String {
-        val inputStream: InputStream = assets.open(textFile)
-        return inputStream.bufferedReader().use { it.readText() }
     }
 }
